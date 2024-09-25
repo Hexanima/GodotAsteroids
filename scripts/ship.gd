0ccc5e8ba4475a505e45e7a1038faa8c6373298e
Hexanima <nicolasfilippelli22@gmail.com>
2024-09-25 11:03:09 -0300

    Added sound files and player ship thrust sound

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -1,11 +1,26 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite
+@onready var propulsion_audio = $PropulsionAudio
+
 const SPEED = 300.0
 const ROTATION_SPEED = 5
 
 func calc_velocity():
 	var vec = Vector2(0,-1).rotated(rotation)
 	return vec
+
+func handle_anim():
+	if Input.is_action_pressed("forward"):
+		sprite.animation = "thrust"
+	else:
+		sprite.animation = "idle"
+		
+func handle_audio():
+	if Input.is_action_just_pressed("forward"):
+		propulsion_audio.play()
+	if Input.is_action_just_released("forward"):
+		propulsion_audio.stop()
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
@@ -16,5 +31,10 @@ func _physics_process(delta):
 		print("ACCELERATING")
 		velocity += calc_velocity() * SPEED * delta
 	velocity -= velocity * delta * 0.5
+	
+	handle_anim()
+	
+	handle_audio()
+	
 	print(velocity)
 	move_and_slide()
