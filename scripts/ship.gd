486fb0198b367887bfea05d042ac502df0038cdd
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-09-25 15:37:02 -0300

    Fixed Player Ship propulsion looping

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -10,19 +10,7 @@ func calc_velocity():
 	var vec = Vector2(0,-1).rotated(rotation)
 	return vec
 
-func handle_anim():
-	if Input.is_action_pressed("forward"):
-		sprite.animation = "thrust"
-	else:
-		sprite.animation = "idle"
-		
-func handle_audio():
-	if Input.is_action_just_pressed("forward"):
-		propulsion_audio.play()
-	if Input.is_action_just_released("forward"):
-		propulsion_audio.stop()
-
-func _physics_process(delta):
+func handle_movement(delta: float):
 	if Input.is_action_pressed("left"):
 		rotation -= ROTATION_SPEED * delta
 	if Input.is_action_pressed("right"):
@@ -31,10 +19,22 @@ func _physics_process(delta):
 		print("ACCELERATING")
 		velocity += calc_velocity() * SPEED * delta
 	velocity -= velocity * delta * 0.5
-	
+
+func handle_anim():
+	if Input.is_action_pressed("forward"):
+		sprite.animation = "thrust"
+	else:
+		sprite.animation = "idle"
+
+func handle_audio():
+	if Input.is_action_just_pressed("forward"):
+		propulsion_audio.play()
+	if Input.is_action_just_released("forward"):
+		propulsion_audio.stop()
+
+func _physics_process(delta):
+	handle_movement(delta)
 	handle_anim()
-	
 	handle_audio()
-	
 	print(velocity)
 	move_and_slide()
